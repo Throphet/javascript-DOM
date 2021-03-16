16f4723a7371e488359efca5cb5680c99f7204bb
Throphet <throphet@gmail.com>
2021-03-16 16:23:45 +0200

    7. uzdevums pabeigts

--- 07-automatic-tabbing-fields/QUESTIONS.md
+++ 07-automatic-tabbing-fields/QUESTIONS.md
@@ -6,4 +6,4 @@
 
 > What are some refactoring techniques you might apply to the provided solution?
 
-Your reply here...
+Šis paņēmiens der tikai noteikti 3 input logiem, taču tā kā mēs iegūstam telefona numuru pēc formāta xxx-xxx-xxxx, tad papildus input logs nebūtu vajadzīgs, taču ja ir jāizpilda personīgā informācija, kur vajag pārlekt uz vairākiem logiem, varētu izmantot form tagu iekš index.html un iegūt visus input elementus ar document.forms\[index], vai ar document.getElementsByTagName("input") sarakstā.
--- 07-automatic-tabbing-fields/global.js
+++ 07-automatic-tabbing-fields/global.js
@@ -1,1 +1,18 @@
 // Your JavaScript goes here.
+window.addEventListener("load", function(){
+    let first = document.getElementById("first");
+    let second = document.getElementById("second");
+    let third = document.getElementById("third");
+
+    first.addEventListener("keyup", function(){
+        if(first.value.length == first.maxLength) {
+            second.focus();
+        }
+    });
+    second.addEventListener("keyup", function(){
+        if(second.value.length == second.maxLength) {
+            third.focus();
+        }
+    });
+
+});
