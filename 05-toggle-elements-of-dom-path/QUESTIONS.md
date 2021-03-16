23a119d22bc798e706051763866b053a8c6e5f69
Throphet <throphet@gmail.com>
2021-03-16 15:28:49 +0200

    5. uzdevums pabeigts

--- 05-toggle-elements-of-dom-path/QUESTIONS.md
+++ 05-toggle-elements-of-dom-path/QUESTIONS.md
@@ -6,4 +6,5 @@
 
 > Describe the contract you used for finding the movies to toggle in the DOM. How is this function different from other functions that find elements in the DOM?
 
-Your reply here...
+Es neieguvu pašus elementus uzreiz, bet gan sākumā ieguvu to parent (vecāku?) elementu. No tā es varēju piekļūt visiem parent children (vecāku bērniem?).
++ document.getElementsByClassName("second_five")[0].children;
--- 05-toggle-elements-of-dom-path/global.js
+++ 05-toggle-elements-of-dom-path/global.js
@@ -1,1 +1,15 @@
 // Your JavaScript goes here.
+window.addEventListener("load", function(){
+    let button = document.getElementById("toggle_button");
+    let toggleElements = document.getElementsByClassName("second_five")[0]; // Paļaujos uz to ka ir tikai viena klase ar nosaukumu "second_five"
+    button.addEventListener("click", function(){
+        Array.from(toggleElements.children).forEach(function(toggle){
+            if(toggle.style.display == "none") {
+                toggle.style.display = "flex";
+            }
+            else {
+                toggle.style.display = "none";
+            }
+        });
+    });
+});
