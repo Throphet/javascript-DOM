667776b01e86de14d57c740cffc75e806203bc80
Throphet <throphet@gmail.com>
2021-03-16 15:14:14 +0200

    4. uzdevums pabeigts

--- 04-toggle-many-elements/global.js
+++ 04-toggle-many-elements/global.js
@@ -1,1 +1,15 @@
 // Your JavaScript goes here.
+window.addEventListener("load", function(){
+    let button = document.getElementById("button");
+    let toggleElements = document.getElementsByClassName("toggle_me");
+    button.addEventListener("click", function(){
+        Array.from(toggleElements).forEach(function(toggle){
+            if(toggle.style.display == "none") {
+                toggle.style.display = "flex";
+            }
+            else {
+                toggle.style.display = "none";
+            }
+        });
+    });
+});
