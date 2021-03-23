76327f2bd9fb2580a8f468e87094fe5d9f6809c7
Throphet <throphet@gmail.com>
2021-03-23 14:28:38 +0200

    Indentation

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,13 +1,13 @@
 // Your JavaScript goes here.
 window.addEventListener("load", function(){
-    let form = document.getElementById("full_name");
-    let output = document.getElementById("greeting");
-    form.addEventListener("keyup", function(){
-        if(form.value.length > 0) {
-            output.textContent = "Labdien, " + form.value + "!";
-        }
-        else {
-            output.textContent = "";
-        }
-    });
+  let form = document.getElementById("full_name");
+  let output = document.getElementById("greeting");
+  form.addEventListener("keyup", function(){
+    if(form.value.length > 0) {
+      output.textContent = "Labdien, " + form.value + "!";
+    }
+    else {
+      output.textContent = "";
+    }
+  });
 });
