1782c4e3ddf22b86ae66895d12d3a371572dc655
Throphet <throphet@gmail.com>
2021-03-23 15:58:45 +0200

    Final changes

--- 05-toggle-elements-of-dom-path/global.js
+++ 05-toggle-elements-of-dom-path/global.js
@@ -1,15 +1,21 @@
 // Your JavaScript goes here.
 window.addEventListener("load", function(){
+
   let button = document.getElementById("toggle_button");
-  let toggleElements = document.getElementsByClassName("second_five")[0]; // Paļaujos uz to ka ir tikai viena klase ar nosaukumu "second_five"
+  let toggleElements = document.getElementsByClassName("second_five")[0];
+  // Paļaujos uz to ka ir tikai viena klase ar nosaukumu "second_five"
+  // Alternatīvs: let toggleElements = document.querySelectorAll(".second_five li");
+
   button.addEventListener("click", function(){
     Array.from(toggleElements.children).forEach(function(toggle){
+
       if(toggle.style.display == "none") {
-        toggle.style.display = "flex";
+        toggle.style.display = "list-item";
       }
       else {
         toggle.style.display = "none";
       }
+
     });
   });
 });
