942052b1028f159cbd0deebb84c7e337b24d9317
Throphet <throphet@gmail.com>
2021-03-23 13:47:16 +0200

    Identation

--- 01-show-one-element/global.js
+++ 01-show-one-element/global.js
@@ -5,23 +5,23 @@ window.addEventListener("load", function(){
   // 2. Add a listener for the 'click' event onto that element.
   showMore.addEventListener("click", function(){
   
-  // 3. The block for the listener should get the DOM
-  //    element containing the text to reveal.
-  let showMoreText = document.getElementById("more_text_content");
-  
-  // 4. Modify that DOM element's style to change it's 'display'
-  //    from a hidden value to a shown value.
-  if(showMoreText.style.display == "none") 
-  {
-    showMoreText.style.display = "inline";
-    showMore.textContent = "Less...";
-  }
-  
-  // 5. Also modify the DOM to hide the "More info..." link.
-  else {
-    showMoreText.style.display = "none";
-    showMore.textContent = "More...";
-  }
+    // 3. The block for the listener should get the DOM
+    //    element containing the text to reveal.
+    let showMoreText = document.getElementById("more_text_content");
     
+    // 4. Modify that DOM element's style to change it's 'display'
+    //    from a hidden value to a shown value.
+    if(showMoreText.style.display == "none") 
+    {
+      showMoreText.style.display = "inline";
+      showMore.textContent = "Less...";
+    }
+    
+    // 5. Also modify the DOM to hide the "More info..." link.
+    else {
+      showMoreText.style.display = "none";
+      showMore.textContent = "More...";
+    }
+      
   });
 });
