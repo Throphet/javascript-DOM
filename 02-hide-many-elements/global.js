504875b272a9db78e1b16bd8185fb5d7102eea60
Throphet <throphet@gmail.com>
2021-03-23 13:58:27 +0200

    Indentation

--- 02-hide-many-elements/global.js
+++ 02-hide-many-elements/global.js
@@ -1,12 +1,9 @@
 window.addEventListener("load", function(){
-
-    let hideButton = document.getElementById("button");
-    let hideElements = document.getElementsByClassName("hide_me");
-    hideButton.addEventListener("click", function(){
-
-        Array.from(hideElements).forEach(function(hide) {
-            hide.style.display = "none";
-        });
-
+  let hideButton = document.getElementById("button");
+  let hideElements = document.getElementsByClassName("hide_me");
+  hideButton.addEventListener("click", function(){
+    Array.from(hideElements).forEach(function(hide) {
+      hide.style.display = "none";
     });
+  });
 });
